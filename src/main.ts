f32fd84cab5ca9beff1dcc3ea296d524884a5669
Gevorg Harutyunyan <gevharut@gmail.com>
2021-10-08 19:55:53 +0400

    Another feature

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css'
 import api from '@/_api'
 import App from './App.vue'
 
-// Init the api and mocks server before mounting the application
+// Init the api and mocks server before mounting the application  
 ;(async () => {
   await api.init()
   createApp(App).mount('#app')
